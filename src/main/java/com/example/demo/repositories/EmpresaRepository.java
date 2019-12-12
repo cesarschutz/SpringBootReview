215e22fec4ebdcc5590e62f65427f5a8896c60ff
Cesa Schutz <cesarschutz340@gmail.com>
2019-12-12 18:29:11 -0300

    criado repositorios e testes dos repositorios

---
+++ src/main/java/com/example/demo/repositories/EmpresaRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.entities.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+	
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+
+}
