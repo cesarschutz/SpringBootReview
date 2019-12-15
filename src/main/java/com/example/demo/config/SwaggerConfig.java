cde30a747985e8280581e9f5d7bb46b9fd5e97cf
Cesa Schutz <cesarschutz340@gmail.com>
2019-12-15 13:28:14 -0300

    criado controller de criacao de pf

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -13,7 +13,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@Profile("dev")
+@Profile({"dev", "hom"})
 @EnableSwagger2
 public class SwaggerConfig {
 
